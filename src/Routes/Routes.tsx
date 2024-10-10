3ef833329511edfa6acfe4d416b11ab2863d196f
yabuJunn <juancho04reyes@gmail.com>
2024-10-10 17:21:40 -0500

    Creacion del router

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Main</div>,
+  },
+  {
+    path: "/detail",
+    element: <div>Detail</div>,
+  },
+])
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
+
 import './index.css'
+import { router } from './Routes/Routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
 )
